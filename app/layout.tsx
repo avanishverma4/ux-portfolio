2329fdcf52532096431ed0f9c5ef7ddcdb1db0ac
Awanish Verma <avanishverma4@gmail.com>
2026-07-30 20:28:16 +0530

    feat: worked on adding clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,6 +35,17 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={manrope.variable}>
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "xul78cyh8y");`,
+          }}
+        />
+      </head>
       <body>
         <Loader />
         <a href="#main-content" className="skip-link">
